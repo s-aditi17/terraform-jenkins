e241dbe39be4aa368ab29058365387341e3a76fa
s-aditi17 <s.aditidinesh17@gmail.com>
2025-07-03 17:54:11 +0530

    Update backend.tf

--- terraform-infra/environment/dev/backend.tf
+++ terraform-infra/environment/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-tf-state-bucket"
+    bucket         = "my-tf-state-tf-bucket"
     key            = "dev/eks.tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf-lock-table"
