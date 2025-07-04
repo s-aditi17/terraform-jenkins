c5d4a4f46fbcda7d8215dc758b1f6d53bd56099c
s-aditi17 <s.aditidinesh17@gmail.com>
2025-07-04 15:26:04 +0530

    Update variables.tf

--- terraform-infra/modules/eks/variables.tf
+++ terraform-infra/modules/eks/variables.tf
@@ -1,10 +1,15 @@
 variable "cluster_name" {}
+
 variable "k8s_version" {}
+
 variable "subnet_ids" {
   type = list(string)
 }
+
 variable "cluster_role_arn" {}
+
 variable "node_role_arn" {}
+
 variable "role_name" {
   description = "IAM Role name for the EKS control plane"
   type        = string
@@ -12,6 +17,11 @@ variable "role_name" {
 
 variable "node_role_name" {
   description = "IAM Role name for the EKS node group"
+  type        = string
+}
+
+variable "assume_role_policy" {
+  description = "IAM trust policy document for EKS roles"
   type        = string
 }
 
