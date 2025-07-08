4d5e0c0e5240bd2677f4d6d616cd3ed46818ff07
s-aditi17 <s.aditidinesh17@gmail.com>
2025-07-08 07:27:15 +0530

    Create provider.tf

---
+++ terraform-infra/environment/dev/provider.tf
@@ -0,0 +1,19 @@
+# environment/dev/provider.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
